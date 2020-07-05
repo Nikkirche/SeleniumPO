22169bcc22f193baa45ee8bde9db342c8d117aa4
Nikkirche <49571700+Nikkirche@users.noreply.github.com>
2020-07-05 22:52:22 +0300

    4.29 passed

--- tests/pages/login_page.py
+++ tests/pages/login_page.py
@@ -1,4 +1,5 @@
 from .base_page import BasePage
+from .locators import LoginPageLocators
 
 
 class LoginPage(BasePage):
@@ -8,13 +9,13 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
-        assert True
+        assert "login" in self.browser.current_url, "word \"login\" not in url"
 
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
-        assert True
+        assert self.is_element_present(*LoginPageLocators.LOGIN_USERNAME) and \
+                self.is_element_present(*LoginPageLocators.LOGIN_PASSWORD), "Login form don't exist"
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
-        assert True
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_EMAIL) and \
+               self.is_element_present(*LoginPageLocators.REGISTRATION_PASSWORD_1) and \
+               self.is_element_present(*LoginPageLocators.REGISTRATION_PASSWORD_2), "Register Form don't exist"
